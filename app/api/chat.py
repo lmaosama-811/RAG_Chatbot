d4788f52039ecc54fcfe625fc73598b5e2b0382d
lmaosama-811 <havietbach.tq@gmail.com>
2026-02-27 22:41:12 +0700

    Enhance chat API with session ID checks
    Added session ID validation and imported CM_service.

--- app/api/chat.py
+++ app/api/chat.py
@@ -9,7 +9,8 @@ from ..model import embeddings, llm
 from ..service.RAG_service import rag_service
 from ..service.LLM_service import llm_service
 from ..service.PDF_service import pdf_service
-from ..deps import check_file_available
+from ..service.CM_service import CM_service
+from ..deps import check_file_available,check_session_id_available
 from ..db import get_session
 
 router = APIRouter(prefix="/chat", tags=["Chat"])
@@ -20,8 +21,14 @@ def chat(request: ChatbotRequest, db: Session = Depend(get_session)):
     You can check whether the file exists and file ID by Get Files"""
     if not check_file_available(request.file_id):
         return Message(message="File not Found")
+    if not check_session_id_available(request.session_id,db):
+        return Message(message="Session ID not Found")
+    if request.session_id is None:
+        pass #create random session_id in CM_service 
     file =pdf_service.process_pdffile(request.file_id) 
     context = rag_service.load_pdf(request.file_id,file,embeddings,request.question) #Get k chunks
+    #create dialog for role user in table 
     output = llm_service.ask_model(task="question_answer",llm=llm,context=context,question=request.question)
-    return ChatBotResponse(model_name="GPT-4o",answer=output.content,count=len(output.content.split()))
+    #create dialog for role assistant in table 
+    return ChatBotResponse(model_name="GPT-4o",answer=output.content,count=len(output.content.split())) #Adjust this 
 

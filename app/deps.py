5499bcdcb5a53901416c0ae8e19f515ceb3f86fe
lmaosama-811 <havietbach.tq@gmail.com>
2026-02-27 22:48:24 +0700

    Add function to check session ID availability
    Added check_session_id_available function to verify session ID.

--- app/deps.py
+++ app/deps.py
@@ -1,5 +1,6 @@
 import os
 from .service.PDF_service import pdf_service
+from .service.DB_service import db_service 
 
 def check_file_available(file_id):
     file_path = pdf_service.get_file_path("upload",file_id)
@@ -7,3 +8,6 @@ def check_file_available(file_id):
 
 def check_file_type(file):
     return file.filename.lower().endswith(".pdf")
+
+def check_session_id_available(session_id,db):
+    return db_service(session_id,db) is not None

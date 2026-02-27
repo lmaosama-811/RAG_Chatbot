9b00e7f5e692d71475d483abf55e8ceb359b0d85
lmaosama-811 <havietbach.tq@gmail.com>
2026-02-27 22:28:54 +0700

    Refactor load_conversation_history method parameters

--- app/service/CM_service.py
+++ app/service/CM_service.py
@@ -7,8 +7,8 @@ class ConversationManagement:
         self.token_threshold = 6000
     def format_history(self,dialog:tuple):
         return {"role":dialog[0],"content":dialog[1]}
-    def load_conversation_history_and_update_summarization(self,session_id,db_history,db_sum,llm,old_summary=None):
-        dialogs = db_service.get_conversation_history(session_id,db_history)
+    def load_conversation_history_and_update_summarization(self,session_id,db,llm,old_summary=None):
+        dialogs = db_service.get_conversation_history(session_id,db)
         count = 0
         recent_dialogs = []
         for i in range(len(dialogs),-1,-1):
@@ -20,7 +20,7 @@ class ConversationManagement:
                 for j in range(i+1):
                     sum_list.append(self.format_history(dialogs[j]))
                 summary = llm_service.ask_model(task="summarization",llm=llm,conversation_history=sum_list,old_summary=old_summary)
-                db_service.create_summary(i+1,summary,db_sum)
+                db_service.create_summary(i+1,summary,db)
                 break 
         return recent_dialogs, {**self.summarizaton,**{"content":summary}} # list[role,content] and role,content
     

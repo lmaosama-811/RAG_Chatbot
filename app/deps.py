26f6d08fe52ed92777582a004637e425748716e6
lmaosama-811 <havietbach.tq@gmail.com>
2026-03-04 08:55:50 +0700

    Enhance file size validation in deps.py
    Added an advanced file size validation method with detailed comments.

--- app/deps.py
+++ app/deps.py
@@ -14,10 +14,25 @@ def check_session_id_available(session_id,db):
     return db_service.get_conversation(session_id, db) is not None 
 
 def validate_file_size(file, max_file_size=settings.max_file_size):
+# Advanced version 
+    file.file.seek(0,2)
+    actual_size=file.file.tell() # trả về int(số bytes)là vị trí con trỏ so với đầu file
+    file.file.seek(0)
+    return actual_size > max_file_size 
+""" Cú pháp đầy đủ của phương thức này là: 
+file.seek(offset, whence)
+offset: Số lượng byte bạn muốn di chuyển con trỏ.
+whence: Điểm mốc để bắt đầu di chuyển (mặc định là 0).
+0: Tính từ đầu file.
+1: Tính từ vị trí hiện tại của con trỏ.
+2: Tính từ cuối file.
+Initial version:
     file_size = 0
     for chunk in file.file:
         file_size += len(chunk)
         if file_size > max_file_size:
+            file.file.seek(0)
             return True 
-    return False 
+    file.file.seek(0)
+    return False """
     
